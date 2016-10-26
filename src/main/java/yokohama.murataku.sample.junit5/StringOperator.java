498eafec03016010ede1d44c2f9b06fd320832d9
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-27 00:09:10 +0900

    Add stub classes

---
+++ src/main/java/yokohama.murataku.sample.junit5/StringOperator.java
@@ -0,0 +1,17 @@
+package yokohama.murataku.sample.junit5;
+
+public class StringOperator {
+
+    private final String joiner;
+
+    public StringOperator() {
+        this.joiner = ",";
+    }
+
+    public StringOperator(String joiner) {
+        this.joiner = joiner;
+    }
+
+
+
+}
---
+++ src/test/java/yokohama/murataku/sample/junit5/StringOperatorTest.java
@@ -0,0 +1,11 @@
+package yokohama.murataku.sample.junit5;
+
+import org.junit.jupiter.api.Test;
+
+public class StringOperatorTest {
+
+    @Test
+    public void testJoin() {
+
+    }
+}
