dd70b121a33f348657c5c914f3c292e4527c0637
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2016-10-27 10:09:30 +0900

    Add message by supplier sample

--- src/test/java/yokohama/murataku/sample/junit5/StringOperatorTest.java
+++ src/test/java/yokohama/murataku/sample/junit5/StringOperatorTest.java
@@ -12,4 +12,10 @@ public class StringOperatorTest {
     public void testJoin1() {
         assertEquals("a,b", new StringOperator().join("a", "b"));
     }
+
+    @Test
+    @DisplayName("メッセージは Supplier<String> で設定できる")
+    public void testJoin2() {
+        assertEquals("a,b", new StringOperator().join("a", "b"), ()->"結合が間違っている!!");
+    }
 }
